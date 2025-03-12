bc690cc85e99ecda65272601f19939d029b8bf84
vikas018 <vkyadav018@gmail.com>
2025-03-12 18:49:01 +0530

    added team member images

--- src/data/team.ts
+++ src/data/team.ts
@@ -1,14 +1,17 @@
+import vikas from '../images/vikas.jpg';
+import dheeraj from '../images/dheeraj.jpg';
+
 export const teamMembers = [
   {
     name: 'Dheeraj Sharma',
     role: 'Chief executive officer',
-    image: 'https://instagram.fdel25-3.fna.fbcdn.net/v/t51.2885-19/70538907_525258698251201_2760258103977967616_n.jpg?stp=dst-jpg_s150x150_tt6&amp;_nc_ht=instagram.fdel25-3.fna.fbcdn.net&amp;_nc_cat=101&amp;_nc_oc=Q6cZ2AF1Cbr6SGtg_rhbVkrrdiOeJB61_qnEptED5NFq1idrjH45vkks615dh2arIKOguHW_2bVPF3FYjMnuOGVS3Kv3&amp;_nc_ohc=cOqRJiJ1eMQQ7kNvgH-EoMe&amp;_nc_gid=ded012b511194339bd1dce3474608d55&amp;edm=AP4sbd4BAAAA&amp;ccb=7-5&amp;oh=00_AYG4KwoiBb5UJtrY16bPivSKyn1YD29zwY30a2jANuWccw&amp;oe=67D5086F&amp;_nc_sid=7a9f4b',
+    image: dheeraj,
     bio: 'With over 12 years of experience in tea cultivation and tasting, Dheeraj brings his passion for Indian teas to every cup.'
   },
   {
     name: 'Vikas Yadav',
     role: 'Chief Commercial Officer',
-    image: 'https://instagram.fdel25-3.fna.fbcdn.net/v/t51.2885-19/70538907_525258698251201_2760258103977967616_n.jpg?stp=dst-jpg_s150x150_tt6&amp;_nc_ht=instagram.fdel25-3.fna.fbcdn.net&amp;_nc_cat=101&amp;_nc_oc=Q6cZ2AF1Cbr6SGtg_rhbVkrrdiOeJB61_qnEptED5NFq1idrjH45vkks615dh2arIKOguHW_2bVPF3FYjMnuOGVS3Kv3&amp;_nc_ohc=cOqRJiJ1eMQQ7kNvgH-EoMe&amp;_nc_gid=ded012b511194339bd1dce3474608d55&amp;edm=AP4sbd4BAAAA&amp;ccb=7-5&amp;oh=00_AYG4KwoiBb5UJtrY16bPivSKyn1YD29zwY30a2jANuWccw&amp;oe=67D5086F&amp;_nc_sid=7a9f4b',
+    image: vikas,
     bio: "Vikas travels across India's tea regions to select the finest harvests and maintain relationships with our partner gardens."
   },
 ];
